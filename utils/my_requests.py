fb64db189874a41a7f95e558710fb72d6dead980
전현준 <guswnsxodlf@gmail.com>
2017-02-17 22:25:08 +0900

    Modify my_requests.py

--- utils/my_requests.py
+++ utils/my_requests.py
@@ -1,28 +1,45 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # Created by JHJ on 2017. 2. 15.
 
 import requests
-import json
+import re
+from bs4 import BeautifulSoup
 
 
-def get_news(query, id, secret):
-    # Request
-    # GET https://openapi.naver.com/v1/search/news.json
+def escape_tags_and_emails(string):
+    # escape tags
+    str1 = re.sub(r'<[^<]+?>', '', string)
 
+    # escape emails
+    str2 = re.sub(r'[^ ]+@.[^ ]+', '', str1)
+
+    return str2
+
+
+def get_news():
     try:
-        response = requests.get(
-            url="https://openapi.naver.com/v1/search/news.json",
-            params={
-                "query": query,
-                "display": "10",
-                "sort": "sim",
-            },
-            headers={
-                "X-Naver-Client-Id": id,
-                "X-Naver-Client-Secret": secret,
-            },
-        )
-        return json.loads(response.content.decode('utf-8'))
+        base_url = 'http://news.naver.com'
+        response = requests.get(base_url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        elements = soup.select('div.section_wide > div > ul > li > a.sm2')
+
+        news_url = base_url + elements[0].get('href')
+        response = requests.get(news_url)
+        soup = BeautifulSoup(response.text, 'lxml')
+        elements = soup.select('div#articleBodyContents')
+        line_length = len(elements[0].contents)
+
+        data = []
+
+        for i, v in enumerate(elements[0].contents):
+            if i in range(9) \
+                    or len(escape_tags_and_emails(v.encode('utf-8').strip())) is 0 \
+                    or i in range(line_length - 6, line_length):
+                continue
+            else:
+                data.append(escape_tags_and_emails(v.encode('utf-8').strip()))
+        return data
+
     except requests.exceptions.RequestException:
         print('HTTP Request failed')
         return False
